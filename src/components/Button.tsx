099dfc4af7336dc98d7be729522f8216b086f8e1
Austin Gatlin <austin@gatlin.io>
2023-08-27 16:55:38 +1000

    Add initial share apis code

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,10 @@
-export const Button = ({ children, onClick }) => {
+export const Button = ({ children, onClick, disabled = false, title = "" }) => {
   return (
     <button
-      className="rounded-lg border-2 border-purple-900 bg-purple-100 p-4 text-purple-900 hover:bg-purple-200"
+      className="rounded-lg border-2 border-purple-900 bg-purple-100 p-4 text-purple-900 hover:bg-purple-200 disabled:cursor-not-allowed disabled:border-gray-900 disabled:bg-gray-100 disabled:text-gray-900"
       onClick={onClick}
+      disabled={disabled}
+      title={title}
     >
       {children}
     </button>
